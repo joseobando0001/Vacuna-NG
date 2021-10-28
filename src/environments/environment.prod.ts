54ca43be7a256c0f5af7edb0fc76294a8cdac8a5
joseobando0001 <31702727+joseobando0001@users.noreply.github.com>
2021-10-28 16:51:46 -0500

    Cambios en los environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://127.0.0.1:8080/VacunaWS/',
-  sso: {
-    serverUrl: 'https://srvcas.espe.edu.ec',
-    clientId: 'vd48aMwTrky0y_U_jOFNH5S83CEa',
-    issuer: '/oauth2endpoints/token',
-    redirectUri: window.location.origin,
-    postredirectUrL: window.location.origin,
-    scope: 'openid profile email',
-    logout: '/oidc/logout',
-    tokenEndpoint: '/oauth2endpoints/token',
-    userinfoEndpoint: '/oauth2/userinfo',
-    authorizationEndpoint: '/oauth2/authorize',
-    jwksEndpoint: '/oauth2/jwks',
-    showDebugInformation: true,
-    requireHttps: false,
-    responseType: 'id_token token'
-  }
+  API_URL: 'https://vacuna-ws.herokuapp.com/VacunaWS/',
+
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -5,21 +5,6 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://127.0.0.1:8080/VacunaWS/',
-  sso: {
-    serverUrl: 'https://srvcas.espe.edu.ec',
-    clientId: 'uK1lnaxRbmauGv0OLjENQL_qAFIa',
-    issuer: '/oauth2endpoints/token',
-    redirectUri: window.location.origin,
-    postredirectUrL: window.location.origin,
-    scope: 'openid profile email',
-    logout: '/oidc/logout',
-    tokenEndpoint: '/oauth2endpoints/token',
-    userinfoEndpoint: '/oauth2/userinfo',
-    authorizationEndpoint: '/oauth2/authorize',
-    jwksEndpoint: '/oauth2/jwks',
-    showDebugInformation: true,
-    requireHttps: false,
-    responseType: 'id_token token'
-  }
+  API_URL: 'https://vacuna-ws.herokuapp.com/VacunaWS/',
+
 };
